6dbf6aab841145382d055cdfb522f8ec24315d67
carlosRmelo <carlos.melo@ufrgs.br>
2023-08-18 15:50:17 -0300

    slopes for normal dyLens

--- slope_distribution.sh
+++ slope_distribution.sh
@@ -1,22 +1,24 @@
-	# JAM slopes
-python Distribution_density_slope.py ./7/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./8/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./9/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./11/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./20/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./56405/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./56406/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./83991/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./83996/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./84010/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./100675/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./116278/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./172209/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./313415/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./341482/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./344595/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./396742/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./414107/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./451938/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./485608/model1/normal/JAM/Analysis_2.5Reff/
-python Distribution_density_slope.py ./545285/model1/normal/JAM/Analysis_2.5Reff/
+	# dyLens slopes
+python Distribution_density_slope.py ./7/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./8/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./9/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./11/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./20/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./56405/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./56406/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./83991/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./83996/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./84010/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./100675/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./116278/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./172209/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./313415/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./341482/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./344595/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./396742/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./414107/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./451938/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./485608/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+python Distribution_density_slope.py ./545285/model1/normal/shear/dyLens/phase5/Analysis_2.5Reff/
+
+
