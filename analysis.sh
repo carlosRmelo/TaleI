8906758db4e952b3c9c1c60be4fca355801f9f00
carlosRmelo <carlos.melo@ufrgs.br>
2023-08-28 15:40:28 -0300

    analyses model2 fiducial

--- analysis.sh
+++
@@ -1,3 +0,0 @@
-python Analysis2.py  ./11/model1/no_shear/Lens/ /home/carlos/Documents/GitHub/my_illustris/TNG50-1-snap84-subhalo11/ --Re --Lens --phase=phase5
-
-python Analysis2.py  ./11/model1/no_shear/Lens/ /home/carlos/Documents/GitHub/my_illustris/TNG50-1-snap84-subhalo11/ --alpha=2.5 --Lens --phase=phase5
