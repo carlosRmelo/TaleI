0a7f8722b6f433c108246be00261e1ed7346eafe
carlosRmelo <carlos.melo@ufrgs.br>
2023-09-18 12:09:07 -0300

    results model2 shear

---
+++ analysis_distribution_shear.sh
@@ -0,0 +1,114 @@
+	## JAM analysis within 2.5Reff
+# python Analysis_Distribution.py ./7/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./8/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo8/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./9/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo9/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./11/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo11/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./20/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo20/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./56405/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56405/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./56406/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56406/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./83991/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83991/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./83996/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83996/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./84010/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo84010/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./100675/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo100675/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./116278/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo116278/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./172209/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo172209/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./313415/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo313415/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./341482/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo341482/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./344595/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo344595/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./396742/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo396742/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./414107/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo414107/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./451938/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo451938/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./485608/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo485608/	--alpha=2.5	--JAM
+# python Analysis_Distribution.py ./545285/model2/normal/JAM/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo545285/	--alpha=2.5	--JAM
+
+		## dyLens shear analysis within 2.5Reff
+python Analysis_Distribution.py ./7/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./8/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo8/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./9/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo9/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./11/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo11/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./20/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo20/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./56405/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56405/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./56406/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56406/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./83991/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83991/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./83996/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83996/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./84010/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo84010/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./100675/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo100675/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./116278/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo116278/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./172209/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo172209/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./313415/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo313415/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./341482/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo341482/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./344595/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo344595/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./396742/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo396742/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./414107/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo414107/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./451938/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo451938/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./485608/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo485608/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./545285/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo545285/	--alpha=2.5	--dyLens	--phase=phase5
+	
+	## dyLens shear analysis within Re
+python Analysis_Distribution.py ./7/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./8/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo8/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./9/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo9/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./11/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo11/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./20/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo20/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./56405/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56405/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./56406/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56406/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./83991/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83991/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./83996/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83996/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./84010/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo84010/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./100675/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo100675/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./116278/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo116278/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./172209/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo172209/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./313415/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo313415/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./341482/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo341482/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./344595/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo344595/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./396742/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo396742/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./414107/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo414107/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./451938/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo451938/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./485608/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo485608/	--Re	--dyLens	--phase=phase5
+python Analysis_Distribution.py ./545285/model2/normal/shear/dyLens/	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo545285/	--Re	--dyLens	--phase=phase5
+
+	## Lens no shear analysis within Re
+# python Analysis_Distribution.py ./7/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./8/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo8/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./9/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo9/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./11/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo11/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./20/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo20/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./56405/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56405/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./56406/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56406/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./83991/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83991/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./83996/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83996/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./84010/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo84010/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./100675/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo100675/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./116278/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo116278/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./172209/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo172209/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./313415/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo313415/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./341482/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo341482/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./344595/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo344595/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./396742/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo396742/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./414107/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo414107/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./451938/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo451938/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./485608/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo485608/	--Re	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./545285/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo545285/	--Re	--Lens	--phase=phase5
+
+	## Lens no shear analysis within 2.5Reff
+# python Analysis_Distribution.py ./7/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./8/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo8/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./9/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo9/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./11/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo11/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./20/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo20/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./56405/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56405/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./56406/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56406/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./83991/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83991/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./83996/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83996/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./84010/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo84010/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./100675/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo100675/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./116278/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo116278/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./172209/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo172209/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./313415/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo313415/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./341482/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo341482/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./344595/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo344595/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./396742/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo396742/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./414107/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo414107/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./451938/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo451938/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./485608/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo485608/	--alpha=2.5	--Lens	--phase=phase5
+# python Analysis_Distribution.py ./545285/model2/normal/shear/Lens/		/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo545285/	--alpha=2.5	--Lens	--phase=phase5
