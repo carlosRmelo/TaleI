cd53e3d166c4aa6d8f1fd737edbeb1cd383d1051
carlosRmelo <carlos.melo@ufrgs.br>
2023-09-25 12:45:36 -0300

    30- redoing analyses

--- bigger_analysis_fiducial.sh
+++ bigger_analysis_fiducial.sh
@@ -1,5 +1,5 @@
 		## dyLens  shear analysis within 2.5Reff
-python Analysis_Fiducial.py ./7/model1/bigger/shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/	--alpha=2.5	--dyLens	--phase=phase5
+#python Analysis_Fiducial.py ./7/model1/bigger/shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/	--alpha=2.5	--dyLens	--phase=phase5
 python Analysis_Fiducial.py ./8/model1/bigger/shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo8/	--alpha=2.5	--dyLens	--phase=phase5
 python Analysis_Fiducial.py ./9/model1/bigger/shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo9/	--alpha=2.5	--dyLens	--phase=phase5
 python Analysis_Fiducial.py ./11/model1/bigger/shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo11/	--alpha=2.5	--dyLens	--phase=phase5
@@ -44,3 +44,48 @@ python Analysis_Fiducial.py ./451938/model1/bigger/shear/dyLens	/home/carlosmelo
 python Analysis_Fiducial.py ./485608/model1/bigger/shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo485608/	--Re	--dyLens	--phase=phase5
 python Analysis_Fiducial.py ./545285/model1/bigger/shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo545285/	--Re	--dyLens	--phase=phase5
 
+## dyLens  no shear analysis within 2.5Reff
+#python Analysis_Fiducial.py ./7/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./8/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo8/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./9/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo9/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./11/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo11/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./20/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo20/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./56405/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56405/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./56406/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56406/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./83991/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83991/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./83996/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83996/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./84010/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo84010/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./100675/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo100675/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./116278/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo116278/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./172209/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo172209/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./313415/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo313415/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./341482/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo341482/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./344595/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo344595/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./396742/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo396742/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./414107/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo414107/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./451938/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo451938/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./485608/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo485608/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./545285/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo545285/bigger/	--alpha=2.5	--dyLens	--phase=phase5
+	
+	## dyLens  no shear analysis within Re
+python Analysis_Fiducial.py ./7/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo7/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./8/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo8/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./9/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo9/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./11/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo11/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./20/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo20/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./56405/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56405/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./56406/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo56406/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./83991/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83991/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./83996/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo83996/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./84010/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo84010/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./100675/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo100675/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./116278/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo116278/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./172209/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo172209/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./313415/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo313415/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./341482/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo341482/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./344595/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo344595/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./396742/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo396742/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./414107/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo414107/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./451938/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo451938/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./485608/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo485608/bigger/	--Re	--dyLens	--phase=phase5
+python Analysis_Fiducial.py ./545285/model1/bigger/no_shear/dyLens	/home/carlosmelo/Documents/GitHub/Illustris/my_illustris/TNG50-1-snap84-subhalo545285/bigger/	--Re	--dyLens	--phase=phase5
