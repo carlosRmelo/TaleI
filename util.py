f0f6dba2f17e01b47238b148c227014085d1be9b
carlosRmelo <carlos.melo@ufrgs.br>
2023-11-07 15:20:25 -0300

    a few tests with AD-test

--- util.py
+++ util.py
@@ -8,6 +8,57 @@ Some useful utilities.
 """
 
 import numpy as np
+from scipy.stats import anderson_ksamp
+
+def bootstrapping_ADtest(sample_A, sample_B, niter: int):
+    """
+        Performs a bootstrapping with replacement to the empirical
+        Anderson-Darling test to estimate a confidence 
+        interval/level to the p-value.
+        This is an option if your samples are quite small.
+        
+        Inputs
+        ----------
+        sample_A, sample_B: np.arrays (1D)
+            numpy arrays with with the data
+        niter: int
+            number of interations for the bootstrapping
+        
+        Output
+        ---------
+        p_dist: np.array
+            numpy array with same size ad niter containing the 
+            p-values for each interation.
+    """
+    p_dist = np.empty(niter)
+    for i in range(niter):
+        A = np.random.choice(sample_A, size=sample_A.size, replace=True)
+        B = np.random.choice(sample_B, size=sample_B.size, replace=True)
+        
+        AD_test   = anderson_ksamp([A, B])
+        p_dist[i] = AD_test.significance_level
+    
+    return p_dist
+
+
+def median_hi_lo(dataset):
+    # Return the median, hi and low percentiles
+    p = np.percentile(dataset, [16, 50, 84])
+    median = p[1]
+    hi, lo = np.diff(p)
+    
+    return median, hi, lo
+
+def return_symbol(significance_level):
+    "given a p-value, return a symbol to be ploted"
+    if significance_level == 0.25:
+        symbol = ">"
+    elif significance_level == 0.01:
+        symbol = "<"
+    else:
+        symbol = "="
+    
+    return symbol
 
 def quantities3D(star_dataset, dm_dataset, info, R_kpc):
     """
@@ -106,3 +157,5 @@ def quantities2D(star_dataset, dm_dataset, info, R_kpc):
     fdm    = Mdm/Mtotal               # DM fraction in the snapshot within R
 
     return Mstar, Mdm, Mbh, Mtotal, fdm
+
+
